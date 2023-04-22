cacd62ef661400af69cd2c2fb9ad8772ae33dcb7
ldydek <91955435+ldydek@users.noreply.github.com>
2023-04-22 17:25:36 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,17 +3,9 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.uix.popup import Popup
 from kivy.uix.button import Button
-from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.uix.image import Image
 from json.decoder import JSONDecodeError
 from kivy.garden.mapview import MapView
-
 
 
 class CarInfoApp(App):    
@@ -31,49 +23,37 @@ class CarInfoApp(App):
             }
 
         # create layout
-        layout = GridLayout(cols=1, spacing=1, padding=20)
-
-        title_label = Label(text="AutoMobile")
-        layout.add_widget(title_label)
-
-        image = Image(source='car.png', allow_stretch=True, keep_ratio=False, size_hint=(4,8))
-        layout.add_widget(image)
-
+        main_grid = GridLayout(cols=1, spacing=10, padding=40)
+        label_grid = GridLayout(cols=2, size_hint=(1, 5))
 
         # create labels and inputs for car info data
-        fuel_label = Label(text="Poziom paliwa:", valign='center')
-        fuel_input = TextInput(text=str(data["fuel"]), size_hint_y=None, height=50)
-        location_label = Label(text="Aktualna lokalizacja:", valign='center')
-        location_input = TextInput(text=str(data["location"]), size_hint_y=None, height=50)
-        oil_label = Label(text="Temperatura oleju:", valign='center')
-        oil_input = TextInput(text=str(data["oil"]), size_hint_y=None, height=50)
-        coolant_label = Label(text="Poziom płynu chłodniczego:", valign='center')
-        coolant_input = TextInput(text=str(data["coolant"]), size_hint_y=None, height=50)
+        fuel_label = Label(text="Poziom paliwa:")
+        fuel_input = TextInput(text=str(data["fuel"]))
+        location_label = Label(text="Aktualna lokalizacja:")
+        location_input = TextInput(text=str(data["location"]))
+        oil_label = Label(text="Stan oleju:")
+        oil_input = TextInput(text=str(data["oil"]))
+        coolant_label = Label(text="Stan płynu chłodniczego:")
+        coolant_input = TextInput(text=str(data["coolant"]))
 
         # add widgets to layout
-        layout.add_widget(fuel_label)
-        layout.add_widget(fuel_input)
-        layout.add_widget(location_label)
-        layout.add_widget(location_input)
-        layout.add_widget(oil_label)
-        layout.add_widget(oil_input)
-        layout.add_widget(coolant_label)
-        layout.add_widget(coolant_input)
+        label_grid.add_widget(fuel_label)
+        label_grid.add_widget(fuel_input)
+        label_grid.add_widget(location_label)
+        label_grid.add_widget(location_input)
+        label_grid.add_widget(oil_label)
+        label_grid.add_widget(oil_input)
+        label_grid.add_widget(coolant_label)
+        label_grid.add_widget(coolant_input)
 
-        # create save button
-        save_button = Button(text='Zapisz')
-        save_button.bind(on_press=lambda x: self.save_data(fuel_input.text, location_input.text,
-                                                          oil_input.text, coolant_input.text))
+        main_grid.add_widget(label_grid)
 
-        # add save button to layout
-        layout.add_widget(Label())  # empty label for spacing
-        layout.add_widget(save_button)
+        mapview = MapView(zoom=11, lat=50.6394, lon=3.057, size_hint=(4, 12))
+        main_grid.add_widget(mapview)
 
-        mapview = MapView(zoom=11, lat=52.374, lon=4.900, size_hint=(4,12))
-        layout.add_widget(mapview)
+        self.create_button_grid(main_grid)
 
-
-        return layout
+        return main_grid
 
     def save_data(self, fuel, location, oil, coolant):
         # create dictionary with car info data
@@ -87,6 +67,16 @@ class CarInfoApp(App):
         # save data to json file
         with open("car_info.json", "w") as f:
             json.dump(data, f)
+
+    def create_button_grid(self, main_grid):
+        button_grid = GridLayout(cols=3, spacing=10, padding=40, size_hint=(1, 3))
+        button1 = Button(text='Przycisk', size_hint=(1, 10))
+        button2 = Button(text='Button2')
+        button3 = Button(text='Button3')
+        button_grid.add_widget(button1)
+        button_grid.add_widget(button2)
+        button_grid.add_widget(button3)
+        main_grid.add_widget(button_grid)
 
 
 if __name__ == '__main__':
